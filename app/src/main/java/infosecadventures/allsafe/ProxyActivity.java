e7572d5b7d8b14a24939eca6e68e35a21bbdf2a0
Shao Doo <ahdoo.ling010519@gmail.com>
2024-06-03 11:15:48 +0800

    Remove hardcoded password in ProxyActivity onCreate method

--- app/src/main/java/infosecadventures/allsafe/ProxyActivity.java
+++ app/src/main/java/infosecadventures/allsafe/ProxyActivity.java
@@ -8,7 +8,6 @@ public class ProxyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        string password = "flag{this_is_a_fake_flag}";
         super.onCreate(savedInstanceState);
         startActivity(getIntent().getParcelableExtra("extra_intent"));
     }

88f5b406a444481cc6d8808a4d5b78d7fec6a2cf
Shao Doo <ahdoo.ling010519@gmail.com>
2024-06-03 11:07:59 +0800

    Fix class cast exception

--- app/src/main/java/infosecadventures/allsafe/ProxyActivity.java
+++ app/src/main/java/infosecadventures/allsafe/ProxyActivity.java
@@ -8,6 +8,7 @@ public class ProxyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        string password = "flag{this_is_a_fake_flag}";
         super.onCreate(savedInstanceState);
         startActivity(getIntent().getParcelableExtra("extra_intent"));
     }
